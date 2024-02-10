3fb6c2d767d5c96267d715729869e1c7a8287d59
Huntertk <taufik.khan65@gmail.com>
2024-02-10 16:29:15 +0530

    Created a test api routes and controller

--- api/index.js
+++ api/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
+import userRoutes from './routes/user.route.js'
 
 mongoose.connect(process.env.MONGO).then((conn) =>{
     console.log("Database is connected to this host :",conn.connection.host)
 }).catch((err) => console.log(err))
 
 const app = express();
+
+app.use("/api/user", userRoutes)
 
 
 app.listen(3000, () => {
