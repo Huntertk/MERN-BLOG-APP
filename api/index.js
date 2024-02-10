1280b43f53bcd2434a1e4b067932d986172b4ed0
Huntertk <taufik.khan65@gmail.com>
2024-02-10 15:22:11 +0530

    Connection completed to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO).then((conn) =>{
+    console.log("Database is connected to this host :",conn.connection.host)
+}).catch((err) => console.log(err))
 
 const app = express();
 
