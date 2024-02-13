a5b1e678d0222cbeac8ba9b268d865487d2965ad
Huntertk <taufik.khan65@gmail.com>
2024-02-13 13:34:51 +0530

    Updating Header section

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    profilePicture:{
+        type:String,
+        default:"https://res.cloudinary.com/drrkaak40/image/upload/v1707751910/user_tbngem.png"
+    }
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
