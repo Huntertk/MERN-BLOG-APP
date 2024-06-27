eb1e7dd373fdb53aaa41b88987da01252c7d06ea
Huntertk <taufik.khan65@gmail.com>
2024-06-27 20:28:37 +0530

    db is connected to application

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+mongoose.connect(process.env.MONGO)
+    .then(() => console.log('db is connected to application'))
+    .catch((err) => console.log(err))
 
 //Express App initialization
 const app = express();
