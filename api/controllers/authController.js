ac743460f990b25a78180d1c168c5983a73a2b51
Huntertk <taufik.khan65@gmail.com>
2024-06-29 14:02:56 +0530

    sign in created

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import { AppError } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
@@ -19,6 +20,38 @@ export const signup = async (req, res, next) => {
     
         await newUser.save();
         res.status(201).json({messgae:"Signup Successful"})
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        return next(new AppError(400,"All fields are required!!!"))
+    }
+    
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(new AppError(401,"Wrong Credentials"))
+        }
+        const validPassword = await bcrypt.compare(password, validUser.password);
+        if(!validPassword){
+            return next(new AppError(401,"Wrong Credentials"))
+        }
+        const {password:pass, ...rest} = validUser._doc;
+
+        const token = jwt.sign({userId:validUser._id}, process.env.JWT_SECRET, {
+            expiresIn:'1d'
+        })
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 1000*60*60*24*1
+        }).json({messgae:"Sign in Successfully", user:rest})
+
     } catch (error) {
         next(error)
     }
